af9a3f0998822b09afb14fe0fd5725475ed55252
ulyssefeillet <feilletulysse@gmail.com>
2022-12-31 14:46:56 +0100

    commit

--- pages/api/getdata.js
+++ pages/api/getdata.js
@@ -1,13 +1,13 @@
 import mysql from "mysql2/promise";
 
 export default async function handler(req, res) {
-/*
 
     const dbconnection = await mysql.createConnection({
-        host: "localhost",
-        database: "ecds",
-        user: "root",
-        password: "",
+        host     : 'ecds-1.c3opxu6xd3jh.eu-west-3.rds.amazonaws.com',
+        user     : 'admin',
+        password : '0HEFgic6tLrxV7SszQ18',
+        port     : '3306',
+        database : 'ecds'
     });
 
     try {
@@ -19,18 +19,5 @@ export default async function handler(req, res) {
         res.status(200).json({ articles: data });
     } catch (error) {
         res.status(500).json({ error: error.message});
-    }*/
-    const connection = await mysql.createConnection({
-        host     : 'ecds-1.c3opxu6xd3jh.eu-west-3.rds.amazonaws.com',
-        user     : 'admin',
-        password : 'toto',
-        port     : '3306'
-      });
-      
-      try {
-        connection.end();
-        res.status(200).json({ res: "database connected" });
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
+    }
 }
